c1d5ff00689d5416fc1dfd93253c79a63f73fa5c
gorapallo <gorapall@villanova.edu>
2025-11-04 15:33:21 -0500

    Added Hello World File

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: Nov 4, 2025
+ *      Author: gavin-orapallo
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
