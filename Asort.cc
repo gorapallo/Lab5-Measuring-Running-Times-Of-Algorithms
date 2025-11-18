037a579ab968822962860989332e94b81000c9c2
gorapallo <gorapall@villanova.edu>
2025-11-18 10:54:21 -0500

    added comments

--- Asort.cc
+++ Asort.cc
@@ -2,13 +2,13 @@
 
 
 void insertionSort(long data[], long n){
-	for (long i = 1; i<n; ++i){
-		long temp = data[i];
-		long j = i;
-		while (j>0 && temp < data[j - 1]){
-			data[j] = data[j-1];
-			--j;
+	for (long i = 1; i<n; ++i){ //loops through the array starting at second element
+		long temp = data[i]; //stores current element in temp
+		long j = i; //set j to the current position
+		while (j>0 && temp < data[j - 1]){ //when not at start and temp is smaller than the previous element
+			data[j] = data[j-1]; //element shifts over 1
+			--j; //j moves to the left
 		}
-		data[j] = temp;
+		data[j] = temp; //temp is put in position
 	}
 }
--- Bsort.cc
+++ Bsort.cc
@@ -2,28 +2,28 @@
 
 
 void heapify(long data[], long n, long i){
-	long largest = i;
-	long left = 2*i+1;
-	long right = 2*i+2;
+	long largest = i; //largest is set
+	long left = 2*i+1; //calculates left child index
+	long right = 2*i+2; //calculates right child index
 
-	if (left < n && data[left] > data[largest])
-		largest=left;
-	if (right < n && data[right] > data[largest])
-		largest = right;
+	if (left < n && data[left] > data[largest]) //if child > root
+		largest=left; //sets largest to left
+	if (right < n && data[right] > data[largest])//if child> root
+		largest = right;//sets largest to right
 
-	if (largest != i) {
-		swap(data[i], data[largest]);
-		heapify(data, n, largest);
+	if (largest != i) { //if largest is not root
+		swap(data[i], data[largest]); //swap root with largest
+		heapify(data, n, largest); //heapify the subtree
 	}
 }
 
 void heapSort(long data[], long n){
-	for (long i = n/2 - 1; i>=0; --i){
-		heapify(data, n, i);
+	for (long i = n/2 - 1; i>=0; --i){ //buulds heap from bottom up
+		heapify(data, n, i); //heapify the nodes
 	}
 
-	for (long i = n - 1; i>0; --i){
-		swap(data[0], data[i]);
-		heapify(data, i, 0);
+	for (long i = n - 1; i>0; --i){ //extract elements from  the heap
+		swap(data[0], data[i]); //move root to the end
+		heapify(data, i, 0); //heapify the reduced heap
 	}
 }
